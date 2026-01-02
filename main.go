d3b8695ae620ac8164c0682ed047f57b4a7a3847
Ali506108 <aliduisen77@gmail.com>
2026-01-02 18:35:09 +0500

    learn some go for gin

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
@@ -18,8 +19,28 @@ func main() {
 	value := helloWorld(answerForFunc)
 	fmt.Println("Value is ", value)
 
+	numberFromFunction := addNumber(4, 7)
+	fmt.Println(numberFromFunction)
+
+	netOperation()
+
 }
 
 func helloWorld(value string) string {
 	return value + " Hello from helloWorld class"
+}
+
+func addNumber(valueOne, valueTwo int) int {
+	return valueOne + valueTwo
+}
+
+func netOperation() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("Response statuse : ", resp.Status)
 }
