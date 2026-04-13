d45364f43612518998b63fb520d754024f6d6148
Ali506108 <aliduisen77@gmail.com>
2026-04-13 22:06:02 +0500

    work with map

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gogoogle
+
+go 1.25.6
--- map/map.go
+++ map/map.go
@@ -6,6 +6,10 @@ import (
 
 func main() {
 
+	kafka := make(map[string]int)
+	kafka["localhost:9092"] = 9092
+	fmt.Println(kafka)
+
 	country := make(map[string]int64)
 
 	country["United Kingdem"] = 1_100
@@ -13,5 +17,31 @@ func main() {
 
 	fmt.Println(country)
 	fmt.Printf("\n country with name %d", country["United Kingdem"])
+
+	delete(country, "United Kingdem")
+
+	fmt.Println("map after deleting ", country)
+
+	clear(country)
+
+	fmt.Println(country)
+
+	my_map := map[string]int{
+		"Alex":      20,
+		"Alan":      20,
+		"Elizabeth": 22,
+		"Mark":      24,
+		"Jesica":    34,
+	}
+
+	for i, v := range my_map {
+		fmt.Printf("My name is %d and i'm %d years old\n", i, v)
+	}
+
+	if my_map == nil {
+		fmt.Println("Error")
+	} else {
+		fmt.Println("Okay")
+	}
 
 }
